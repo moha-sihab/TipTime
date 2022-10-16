b051213470a1a7298e45a721c9742c8205ba0cb9
moha-sihab <seehabb@gmail.com>
2022-10-17 06:02:46 +0700

    add UI and Unit Test

---
+++ app/src/androidTest/java/com/sihabudin/tiptime/TipUiTest.kt
@@ -0,0 +1,25 @@
+package com.sihabudin.tiptime
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import com.sihabudin.tiptime.ui.theme.TipTimeTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipUiTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Cost of Service").performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        composeTestRule.onNodeWithText("Tip amount: $2.00").assertExists()
+    }
+}
